33dfdf1f7728e30f552efdf78b07f70f0c81fd4c
Anshul Rastogi <ans7991@gmail.com>
2018-04-01 02:00:10 +0530

    rename variables in kadane's algo

--- src/main/java/algorithm/MaximumSubArraySum.java
+++ src/main/java/algorithm/MaximumSubArraySum.java
@@ -3,13 +3,13 @@ package algorithm;
 public class MaximumSubArraySum {
     //Kadane's algorithm
     int calculate(int[] arr) {
-        int max = arr[0];
-        int maxG = arr[0];
+        int localMaximumSum = arr[0];
+        int globalMaximumSum = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            max = Math.max(arr[i], max + arr[i]);
-            if (max > maxG)
-                maxG = max;
+            localMaximumSum = Math.max(arr[i], localMaximumSum + arr[i]);
+            if (localMaximumSum > globalMaximumSum)
+                globalMaximumSum = localMaximumSum;
         }
-        return maxG;
+        return globalMaximumSum;
     }
 }
